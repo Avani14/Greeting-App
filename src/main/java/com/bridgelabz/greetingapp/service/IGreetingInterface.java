7128f6e39b3a2acdb2473daf94983e7cbe3c6ae3
Avani14 <trivediavani14@gmail.com>
2022-08-06 14:14:04 +0530

    First name and last name with the greeting message.

--- src/main/java/com/bridgelabz/greetingapp/service/IGreetingInterface.java
+++ src/main/java/com/bridgelabz/greetingapp/service/IGreetingInterface.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.greetingapp.service;
 
 import com.bridgelabz.greetingapp.entity.Greeting;
+import com.bridgelabz.greetingapp.entity.Person;
 
 public interface IGreetingInterface {
     Greeting message();
